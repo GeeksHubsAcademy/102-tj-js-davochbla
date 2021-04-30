4a26263868063ce6293ed8e8d5d50c6c8e9a920b
Dave86dev <davidochando1986@gmail.com>
2021-04-30 12:23:03 +0200

    Prueba realizada

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+
+Considere el siguiente problema:
+
+    Escriba un programa corto que permita calcular los números primos dado un input de entrada.
+    
+     N = 13 
+  
+     Resultado :
+	 
+     [2,3,5,7,11,13]
+
+
+    Observe que su resultado es una array de una dimensión.
+    El valor mínimo de entrada será 1, en este caso el resultado devolverá -1.    
+    
+    Se atiente al siguiente ejemplo:
+   
+    N = 1     N = 2      N = 5         N = 8         N = 14     ...    N
+                
+    Resultado:
+
+	-1	   [2]	     [2,3,5]      [2,3,5,7]     [2,3,5,7,11,13]
+
+*/
+
+
+
+
--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,34 @@
 function numPrimo(input) {
- return null;
+
+    if(input == 1){
+        return -1;
+    }else {
+       
+    let Resultado = [];
+    
+    const esPrimo = (numero) => {
+    
+        for(let x = 2; x < numero; x++){
+            if( numero % x === 0){
+    
+                return false;
+    
+            };
+        };
+    
+        return numero;
+    };
+    
+    
+        for (let i = 2; i <= input; i++){
+            if(esPrimo(i)){
+                Resultado.push(i);
+            }
+        };
+        
+        return Resultado;
+    };
+    
 }
 
 module.exports = numPrimo;
